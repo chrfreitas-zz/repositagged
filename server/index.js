1cf6257b22310208d0d3408358d06486d7f27a83
Christian Freitas <chrfreitas@gmail.com>
2018-12-01 19:15:51 -0200

    Create connection between mongo and hapi

--- server/index.js
+++ server/index.js
@@ -1,10 +1,38 @@
 'use strict'
 const Hapi = require('hapi')
+const HapiMongoDB = require('hapi-mongodb')
 
-const server = new Hapi.server({
-  port: 3000,
-  host: 'localhost'
-})
+
+const start = async () => {
+  const server = new Hapi.server({
+    port: 3000,
+    host: 'localhost'
+  })
   
-server.start()
-console.log(`Server running at PORT ${server.info.port} ...`)
+  await server.register({
+    plugin: HapiMongoDB,
+    options: {
+      url: 'mongodb://127.0.0.1:27017/repositagged',
+      decorate: true,
+      settings: {
+        poolSize: 10,
+        useNewUrlParser: true
+      },
+    }
+  })
+  
+  server.route({
+    method: 'GET',
+    path: '/repositories',
+    handler: (request) => {
+      const db = request.mongo.db
+      return db.collection('repositories').find().toArray()
+    }
+  })
+    
+  await server.start()
+  console.log(`Server running at PORT ${server.info.port} ...`)
+}
+
+
+start();
