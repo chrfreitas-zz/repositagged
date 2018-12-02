34b6a9f9e8ba133a6df5805c7f25b1854f6e1afd
Christian Freitas <chrfreitas@gmail.com>
2018-12-02 10:18:48 -0200

    Create route for create and update repository

--- server/classes/repository.js
+++ server/classes/repository.js
@@ -1,13 +1,14 @@
 class Repository {
-  constructor(id, name, description, url, language) {
+  constructor({ id, name, description, url, language, tags }) {
     this.id = id
     this.name = name
     this.description = description
     this.url = url
     this.language = language
+    this.tagged = !!tags || false
     this.createdAt = Date.now()
     this.updatedAt = Date.now()
-    this.tags = []
+    this.tags = tags || []
   }
 }
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -5,31 +5,64 @@ const routes = [
   {
     method: 'GET',
     path: '/repositories',
-    handler: async (request) => {
-      const db = request.mongo.db;
+    handler: async (request, h) => {
+      const { db } = request.mongo;
 
       const dbRepos = await db.collection('repositories').find().toArray()
       const apiRepos = await api.fetchRespositories()
 
-      const result = [];
-      
-      apiRepos.forEach(apiRepo => {
-        dbRepos.forEach(dbRepos => {
-          if(apiRepo.id === dbRepos.id){
-            return result.push(dbRepo)
-          }
-
-          const newRepo = new Repository(
-            apiRepo.id,
-            apiRepo.name,
-            apiRepo.description,
-            apiRepo.html_url,
-            apiRepo.language)
-          return result.push(newRepo)
+      const apiRepoSerialized = apiRepos.reduce((total, current) => {
+        const repository = new Repository({
+          id: current.id,
+          name: current.name,
+          description: current.description,
+          url: current.html_url,
+          language: current.language
         })
+
+        total.push(repository)
+
+        return total
+      }, [])
+
+      const data = [
+        ...apiRepoSerialized,
+        ...dbRepos
+      ]
+
+      return h.response(data).code(200);
+    }
+  },
+  {
+    method: 'POST',
+    path: '/repositories/create',
+    handler: (request, h) => {
+      const repo = new Repository(request.payload)
+
+      const { db } = request.mongo;
+      db.collection('repositories').insertOne(repo)
+
+      return h.response({}).code(200);
+    }
+  },
+  {
+    method: 'POST',
+    path: '/repositories/update',
+    handler: (request, h) => {
+      const { db } = request.mongo;
+
+      const { id, tags } = request.payload;
+
+      db.collection('repositories').updateOne({
+        id,
+      }, {
+        $set: {
+          tags,
+          updatedAt: Date.now(),
+        }
       })
 
-      return result;
+      return h.response().code(200);
     }
   }
 ]
