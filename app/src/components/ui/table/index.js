7ee0b1b8b6c7647d65d1aae76edadfe2e90769d6
Christian Freitas <chrfreitas@gmail.com>
2018-12-02 17:24:28 -0200

    Create list repositories page

---
+++ app/src/components/ui/table/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './index.scss';
+
+const Table = ({ header, body }) => (
+  <table className="table">
+    <thead>
+      <tr>
+        {header.map(item => (
+          <th>{item}</th>
+        ))}
+      </tr>
+    </thead>
+    <tbody>
+      {body.map(item => (
+        <tr>
+          <td>{item.name}</td>
+          <td>{item.description}</td>
+          <td>{item.language}</td>
+          <td>{item.tags}</td>
+        </tr>
+      ))}
+      <tr>
+        <td>
+          <a href="/test">edit</a>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+);
+
+Table.propTypes = {
+  header: PropTypes.array.isRequired,
+  body: PropTypes.array.isRequired,
+};
+
+export default Table;
--- app/src/views/repositories/index.js
+++ app/src/views/repositories/index.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import Progressbar from '../../components/progressbar';
+import Input from '../../components/ui/input';
+import Table from '../../components/ui/table';
+import './index.scss';
+
+const isLoading = false;
+
+const header = ['Repository', 'Description', 'Language', 'Tags', ''];
+
+const repositories = [
+  {
+    name: 'kubernetes',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, nihil.s',
+    language: 'go',
+    tag: '',
+  },
+  {
+    name: 'django',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, nihil.s',
+    language: 'python',
+    tag: '',
+  },
+  {
+    name: 'jest',
+    description:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam, nihil.s',
+    language: 'javascript',
+    tag: '',
+  },
+];
 
 const Repositories = () => (
-  <Progressbar text="Getting the repositories list from Github" />
+  <Fragment>
+    {isLoading && (
+      <Progressbar text="Getting the repositories list from Github" />
+    )}
+    <div className="repositories">
+      <Input placehold="search by tag" />
+      <Table header={header} body={repositories} />
+    </div>
+  </Fragment>
 );
 
 export default Repositories;
