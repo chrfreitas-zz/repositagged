6bf495873e09727eb54d801b10f9aa116b7574e7
Christian Freitas <chrfreitas@gmail.com>
2018-12-04 18:56:15 -0200

    Test change state

--- app/src/views/home/__tests__/home-views.test.js
+++ app/src/views/home/__tests__/home-views.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
 import Home from '../index';
 
 describe('Home view', () => {
@@ -7,5 +8,16 @@ describe('Home view', () => {
     const renderer = new ShallowRenderer();
     const result = renderer.render(<Home />);
     expect(result).toMatchSnapshot();
+  });
+
+  it('should change state username', () => {
+    const evt = {
+      target: { value: 'tarantino' },
+    };
+
+    const wrapper = shallow(<Home />);
+    wrapper.instance().onChangeInput(evt);
+
+    expect(wrapper.state('username')).toBe(evt.target.value);
   });
 });
