7c82413f87ddebef6078e3496e7b23596e2479b7
Christian Freitas <chrfreitas@gmail.com>
2018-12-03 22:29:22 -0200

    Create unit test to actions

---
+++ app/src/actions/__tests__/actions.test.js
@@ -0,0 +1,23 @@
+import actions from '..';
+
+describe('Actions', () => {
+  it('should call return data and FETCH_REPOSITORIES_LIST', () => {
+    const result = actions.fetchRepositoriesList('username');
+    const actionExpected = {
+      type: actions.FETCH_REPOSITORIES_LIST,
+      data: 'username',
+    };
+
+    expect(result).toEqual(actionExpected);
+  });
+
+  it('should call return data and FETCH_REPOSITORIES_LIST_SUCCESS', () => {
+    const result = actions.fetchRepositoriesListSuccess([]);
+    const actionExpected = {
+      type: actions.FETCH_REPOSITORIES_LIST_SUCCESS,
+      data: [],
+    };
+
+    expect(result).toEqual(actionExpected);
+  });
+});
