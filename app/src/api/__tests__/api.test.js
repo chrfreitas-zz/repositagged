3a993b8acecbddd6ea4d0b5c3742cee64d1d9f75
Christian Freitas <chrfreitas@gmail.com>
2018-12-04 20:52:19 -0200

    Create request to api to change the tags

--- app/src/api/__tests__/api.test.js
+++ app/src/api/__tests__/api.test.js
@@ -1,11 +1,31 @@
 import api, { BASE_URL } from '..';
 
 describe('Api', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+
   it('should call fetch with username in the url', () => {
     const username = 'spikejonze';
     api.getRepositories(username);
 
     const url = `${BASE_URL}/repositories/${username}`;
     expect(fetch).toHaveBeenCalledWith(url, { method: 'GET' });
+  });
+
+  it('should call edit tags to update repository with new tags', () => {
+    const id = 1;
+    const tags = 'elixir';
+
+    api.editTags(id, tags);
+
+    const url = `${BASE_URL}/repositories/update`;
+    expect(fetch).toHaveBeenCalledWith(url, {
+      method: 'POST',
+      body: {
+        id,
+        tags,
+      },
+    });
   });
 });
--- app/src/api/index.js
+++ app/src/api/index.js
@@ -1,13 +1,26 @@
 export const BASE_URL = 'http://localhost:9000';
 
-function getRepositories(username) {
+const getRepositories = (username) => {
   const url = `${BASE_URL}/repositories/${username}`;
 
   return fetch(url, {
     method: 'GET',
   }).then(response => response.json());
-}
+};
+
+const editTags = (id, tags) => {
+  const url = `${BASE_URL}/repositories/update`;
+
+  return fetch(url, {
+    method: 'POST',
+    body: {
+      id,
+      tags,
+    },
+  }).then(response => response.json());
+};
 
 export default {
   getRepositories,
+  editTags,
 };
