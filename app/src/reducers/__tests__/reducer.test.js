2b266946c186fd682ea0fda5a280d1c8d15c54b8
Christian Freitas <chrfreitas@gmail.com>
2018-12-03 22:38:43 -0200

    Create unit test for reducer

---
+++ app/src/reducers/__tests__/reducer.test.js
@@ -0,0 +1,36 @@
+import reducer from '..';
+import actions from '../../actions';
+
+describe('Reducer', () => {
+  it('should return initial state', () => {
+    const initialState = {
+      repositories: [],
+    };
+
+    const state = initialState;
+
+    const action = {
+      type: 'TEST',
+    };
+
+    const result = reducer(state, action);
+    expect(result).toBe(initialState);
+  });
+
+  it('should return a list of repositories', () => {
+    const state = {
+      repositories: [],
+    };
+
+    const action = {
+      type: actions.FETCH_REPOSITORIES_LIST_SUCCESS,
+      data: [{
+        name: 'mongodb',
+        description: 'mongodb repository',
+      }],
+    };
+
+    const result = reducer(state, action);
+    expect(result.repositories).toBe(action.data);
+  });
+});
